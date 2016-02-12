cf7bebb77cab71dcdbf26f2fb3082b28cd1f248a
Marco Amadori <marco_amadori@yahoo.it>
2016-02-12 16:16:46 +0100

    added get cart event

--- public/js/app/vivocha/vivocha-index.js
+++ public/js/app/vivocha/vivocha-index.js
@@ -1,19 +1,58 @@
 'use strict';
 angular
     .module('vvc',[])
-    .constant('vvc_acct_name','vvc_demo')
-    .controller('vvcTest',['$scope','$rootScope',function($scope,$rootScope){
+    .constant('vvc_acct_name','yaasdemo')
+    .directive('vvcScript',['vvc_acct_name','$window','$rootScope',function(vvc_acct_name,$window,$rootScope) {
+      return {
+        restrict: 'A',
+        link: function () {
+          console.log("linking vvcScript",vvc_acct_name);
+          window.vvc_ready_handlers = [];
+          window.vivocha = { ready: function(cb) { window.vvc_ready_handlers.push(cb); }}
+
+          var head = document.getElementsByTagName("head")[0];
+          var js = document.createElement("script");
+          js.type = "text/javascript";
+          js.src = "//www.vivocha.com/a/"+vvc_acct_name+"/api/vivocha.js";
+          head.appendChild(js);
+          //$window.vvc_ready_handlers = [];
+          //$window.vivocha = { ready: function(cb) { $window.vvc_ready_handlers.push(cb); }}
+          //$rootScope.showCart = true;
+
+        }
+      }
+    }])
+    .controller('vvcTest',['$scope','$rootScope','$window','$timeout',function($scope,$rootScope,$window,$timeout){
+
+      var theCart = {};
+
+      $timeout(function(){
+        var vivocha = $window.vivocha;
+
+        vivocha.ready(function(){
+          console.log("READY CALLED");
+
+          vivocha.events.on("contact",function(contact){
+            console.log("CONTACT CALLED");
+            contact.on("cart:get",function(data,cb){
+              cb(null,JSON.parse(angular.toJson(theCart)));
+            })
+          })
+
+        })
+      },500);
 
       $rootScope.$on('cart:updated',function(event,obj){
-        vivocha_send("cart_updated",obj.cart);
+        theCart = obj.cart;
+        vivocha_send("cart:updated",obj.cart);
       });
 
       $rootScope.$on('user:signedin',function(event,obj) {
-        vivocha_send("user_signedin",obj);
+        vivocha_send("user:signedin",obj);
       });
 
       $rootScope.$on('user:signedout',function(event,obj) {
-        vivocha_send("user_signedout",obj);
+        vivocha_send("user:signedout",obj);
       });
 
       function vivocha_send(evt,obj){
@@ -29,18 +68,5 @@ angular
       }
 
     }])
-    .directive('vvcScript',['vvc_acct_name',function(vvc_acct_name) {
-      return {
-        restrict: 'A',
-        link: function () {
-          console.log("linking vvcScript",vvc_acct_name);
-          var head = document.getElementsByTagName("head")[0];
-          var js = document.createElement("script");
-          js.type = "text/javascript";
-          js.src = "//www.vivocha.com/a/"+vvc_acct_name+"/api/vivocha.js";
-          head.appendChild(js);
 
-        }
-      }
-    }])
 ;
